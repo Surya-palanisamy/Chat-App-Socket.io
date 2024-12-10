398b54bf2408a36e7309f0f13982e2b3c8562d22
Surya-palanisamy <suryasitit@gmail.com>
2024-12-10 18:05:30 +0530

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Join from "./components/login";
-import Chat from "./components/chat";
+import Chat from "./components/Chat";
 const App = () => {
   return (
     <Router>
